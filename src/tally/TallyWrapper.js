040d25ad471d053423cba12e70edce98df0cf80d
Emmanuel Morales <emmanuel.morales@headspring.com>
2018-04-05 15:15:38 -0500

    Add form for adding a category not functional at this time

--- src/tally/TallyWrapper.js
+++ src/tally/TallyWrapper.js
@@ -8,7 +8,8 @@ class TallyWrapper extends React.Component {
       items: [
         { label: 'Water', currentValue: 0, defaultValue: 8, id: '8038a70a-21b6-592b-9e12-e055dca7b8bf', fact: 'Water makes up approximately 70% of a human’s body weight – but DON’T stop drinking water to lose weight!' },
         { label: 'Veggies', currentValue: 0, defaultValue: 5, id: '15e4db73-08a2-56d3-8ccb-85b767016f49', fact: 'Broccoli contains more protein than steak!' }
-      ]
+      ],
+      formVisible: false
     };
   }
 
@@ -36,13 +37,22 @@ class TallyWrapper extends React.Component {
     });
   }
 
+  handleNewCategoryClick = () =>
+    this.setState({ formVisible: true });
+
+  handleCancelForm = () =>
+  this.setState({ formVisible: false });
+
   render() {
     return (
       <div className="container">
         <Tally
           items={this.state.items}
+          formVisible={this.state.formVisible}
           onAddClick={this.handleAddClick}
           onRemoveClick={this.handleRemoveClick}
+          onNewCategory={this.handleNewCategoryClick}
+          onCancelForm={this.handleCancelForm}
         />
       </div>
     );
