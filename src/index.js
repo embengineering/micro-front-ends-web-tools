f0283fc9e1bf0240e640da341305f0681c549e1a
Emmanuel Morales <emmanuel.morales@headspring.com>
2018-04-10 15:23:07 -0500

    Remove service worker

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import './index.css';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-import registerServiceWorker from './registerServiceWorker';
 import { loadManifest } from './utilities';
 import App from './App';
 
-registerServiceWorker();
 loadManifest('/micro-front-ends-standard-nav-bar');
 loadManifest('/micro-front-ends-tally');
 loadManifest('/micro-front-ends-error-pages');
