ca47c6214f3be4110ac331c4eae47ae96062ee74
Emmanuel Morales <emmanuel.morales@headspring.com>
2018-04-09 20:59:01 -0500

    Add proxy for local development

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import registerServiceWorker from './registerServiceWorker';
 import { loadManifest } from './utilities';
 
 registerServiceWorker();
-loadManifest('http://embengineering.com/micro-front-ends-standard-nav-bar');
-loadManifest('http://embengineering.com/micro-front-ends-tally');
+loadManifest('/micro-front-ends-standard-nav-bar');
+loadManifest('/micro-front-ends-tally');
