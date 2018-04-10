938a3015f650dc04dc36e15abc2a3bfa3d363031
Emmanuel Morales <emmanuel.morales@headspring.com>
2018-04-10 16:36:34 -0500

    Display in console PUBLIC_URL for testing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+console.log(`PUBLIC_URL: ${process.env.PUBLIC_URL}`);
 
 const App = () =>
   <Router>
