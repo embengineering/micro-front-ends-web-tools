ebd4e40bbc297b2c579d125b2ee4f20ad8203199
Emmanuel Morales <emmanuel.morales@headspring.com>
2018-04-11 11:34:28 -0500

    Remove react router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import './index.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
 import { loadManifest } from './utilities';
-import App from './App';
 
 loadManifest('/micro-front-ends-standard-nav-bar');
 loadManifest('/micro-front-ends-tally');
 loadManifest('/micro-front-ends-error-pages');
-
-ReactDOM.render(<App />, document.querySelector('#main'));
